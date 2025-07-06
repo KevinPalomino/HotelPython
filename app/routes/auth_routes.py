17a53de5ce9748be2428728418ac1506b6abf9bf
KevinPalomino <kevinpalomino.jolianis@gmail.com>
2025-07-06 12:51:58 -0500

    Se agregó "editar contraseña" desde la vista de cada usuario.

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,10 +1,10 @@
 # app/routes/auth_routes.py
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from app.models.models import Persona
 from app import db
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -46,3 +46,31 @@ def logout():
     logout_user()
     flash("Sesión cerrada", "success")
     return redirect(url_for('auth.login'))
+
+
+@auth_bp.route('/mi-cuenta/cambiar-clave', methods=['GET', 'POST'])
+@login_required
+def cambiar_clave_personal():
+    if request.method == 'POST':
+        actual = request.form['actual']
+        nueva = request.form['nueva']
+        confirmar = request.form['confirmar']
+
+        if not check_password_hash(current_user.contrasena, actual):
+            flash("La contraseña actual es incorrecta.", "danger")
+            return redirect(url_for('auth.cambiar_clave_personal'))
+
+        if nueva != confirmar:
+            flash("Las nuevas contraseñas no coinciden.", "danger")
+            return redirect(url_for('auth.cambiar_clave_personal'))
+
+        if len(nueva) < 6:
+            flash("La nueva contraseña debe tener al menos 6 caracteres.", "danger")
+            return redirect(url_for('auth.cambiar_clave_personal'))
+
+        current_user.contrasena = generate_password_hash(nueva)
+        db.session.commit()
+        flash("Contraseña actualizada exitosamente.", "success")
+        return redirect(url_for('admin.panel_admin'))
+
+    return render_template('auth/cambiar_contrasena_personal.html')
